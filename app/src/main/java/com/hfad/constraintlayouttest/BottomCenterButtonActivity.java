081eec8dfb6d32f2c95bf9c2ef53fd7be3eee34c
Vathanak Mao <vathanakmao@gmail.com>
2023-12-31 16:07:24 +0700

    Added BottomCenterButtonActivity

---
+++ app/src/main/java/com/hfad/constraintlayouttest/BottomCenterButtonActivity.java
@@ -0,0 +1,14 @@
+package com.hfad.constraintlayouttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BottomCenterButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_center_button);
+    }
+}
