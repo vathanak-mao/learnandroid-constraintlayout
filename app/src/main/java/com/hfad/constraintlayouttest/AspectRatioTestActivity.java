15e6fbd6b29c2e0e516ff7a6a80e1a7dcf61cc35
Vathanak Mao <vathanakmao@gmail.com>
2024-01-08 17:14:28 +0700

    Added AspectRatioTestActivity

---
+++ app/src/main/java/com/hfad/constraintlayouttest/AspectRatioTestActivity.java
@@ -0,0 +1,14 @@
+package com.hfad.constraintlayouttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AspectRatioTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aspect_ratio_test);
+    }
+}
