8d2c403d5124452e5e8443f4428ca106baa44ef1
Vathanak Mao <vathanakmao@gmail.com>
2024-01-12 16:42:13 +0700

    Added AlignViewsTestActivity

---
+++ app/src/main/java/com/hfad/constraintlayouttest/AlignViewsTestActivity.java
@@ -0,0 +1,14 @@
+package com.hfad.constraintlayouttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlignViewsTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_align_views_test);
+    }
+}
